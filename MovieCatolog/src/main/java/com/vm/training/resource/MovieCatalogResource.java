80b11ace5aba7e4384aec5bfa27228aab4c700c5
sasi chowdary <sasikumargadiparthi319@gmail.com>
2021-06-26 11:58:48 +0530

    initial commit

--- MovieCatolog/src/main/java/com/vm/training/resource/MovieCatalogResource.java
+++ MovieCatolog/src/main/java/com/vm/training/resource/MovieCatalogResource.java
@@ -1,5 +1,28 @@
 package com.vm.training.resource;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.vm.training.model.CatalogItems;
+
+
+@RestController
+@RequestMapping("/catalog")
 public class MovieCatalogResource {
+
+	
+	@RequestMapping("/{userId}")
+	public List<CatalogItems> getCatalog(@PathVariable("userId") String userId) {
+		return Arrays.asList(
+				new CatalogItems("firstmovie", "first movie desc", 1),
+				new CatalogItems("secondmovie", "second movie desc", 2)
+
+				);
+
+	}
 
 }
