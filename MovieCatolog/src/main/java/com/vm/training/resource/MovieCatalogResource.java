7069717c68f6838efbdf78698fbe290bd4fd3c31
sasi chowdary <sasikumargadiparthi319@gmail.com>
2021-06-26 14:25:55 +0530

    hardcoded---added rating class and geting the data from rating class using Streams&filters

--- MovieCatolog/src/main/java/com/vm/training/resource/MovieCatalogResource.java
+++ MovieCatolog/src/main/java/com/vm/training/resource/MovieCatalogResource.java
@@ -2,12 +2,14 @@ package com.vm.training.resource;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.vm.training.model.CatalogItems;
+import com.vm.training.model.Rating;
 
 
 @RestController
@@ -17,11 +19,27 @@ public class MovieCatalogResource {
 	
 	@RequestMapping("/{userId}")
 	public List<CatalogItems> getCatalog(@PathVariable("userId") String userId) {
-		return Arrays.asList(
-				new CatalogItems("firstmovie", "first movie desc", 1),
-				new CatalogItems("secondmovie", "second movie desc", 2)
-
+		//1. get all the rated movie ids freom the ratings service
+		List<Rating> ratingsBag = Arrays.asList(  //mocking call to ratings ms
+				new Rating("movieOne",1),
+				new Rating("movieTwo",2),
+				new Rating("movieThree",3)
 				);
+		
+		
+		//for each movie id retrieved from the previous MS call send it to MovieInfo and get movie details
+		return ratingsBag.stream()                                                  //putting the list of items [emptying the bag on a] conveyor belt
+				.map(rating -> new CatalogItems(rating.getMovieId(), "description of "+ rating.getMovieId(), rating.getRating()))   //converting each rating item moving on the conveyor belt TO  catalogitem
+				.collect(Collectors.toList());                                                                                     //after all the items have been converted put it back into the bag[list]
+		
+		//put them together and serve the user
+		
+		/*
+		 * return Arrays.asList( new CatalogItem("firstmovie", "first movie desc", 1),
+		 * new CatalogItem("secondmovie", "second movie desc", 2)
+		 * 
+		 * );
+		 */
 
 	}
 
